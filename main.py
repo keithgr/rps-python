68063944785af8aed3a2f4958e6493349c914627
Keith Grable <kw28grab@siena.edu>
2019-05-03 19:11:22 -0400

    Implementing rule

--- main.py
+++ main.py
@@ -7,11 +7,24 @@ N = 10
 """ 
     Initialize results array 
     Row reps number of agents
-    Column reps number of enemies 
+    Column reps number of enemies
 """
 results = N * [N * [-1]]
 
-#print(results)
+""" Rule """
+def rule(mcs):
+    if mcs[0] < mcs[1]:
+        if mcs[0] < mcs[2]: return [0, 0, 1]
+        else: return [0, 1, 0]
+    elif mcs[0] > mcs[1]:
+        if mcs[1] <= mcs[2]: return [1, 0, 0]
+        else: [0, 1, 0]
+    else:
+        if mcs[0] < mcs[2]: return [0, 0, 1]
+        elif mcs[0] > mcs[2]: return [0, 1, 0]
+        else: return [1, 1, 1]
+
+
 
 """ For each player count """
 for n in range(1, N + 1):
@@ -25,8 +38,13 @@ for n in range(1, N + 1):
             For each agent strategy 
             WLOG, rock is the least frequent move
         """
-        for r in range(0, math.floor(a / 3) + 1):
-            for p in range(r, a - 2 * r + 1):
-                s = a - r - p
+        for ra in range(0, math.floor(a / 3) + 1):
+            for pa in range(ra, a - 2 * ra + 1):
+                s = a - ra - pa
 
-                #print('{}\t{}-{}-{}'.format(a, r, p, s));
+                """ For each enemy configuration """
+                for re in range(0, e + 1):
+                    for pe in range(0, e - re + 1):
+                        se = e - re - pe
+
+
