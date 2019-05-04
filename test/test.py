5d808101245077c3602c2ba67de6758297d533cf
Keith Grable <kw28grab@siena.edu>
2019-05-03 20:17:05 -0400

    Installed ddt testing package

--- test/test.py
+++ test/test.py
@@ -1,22 +1,34 @@
 
 import unittest
-from main import rule
+from ddt import data, unpack
+from main.rule import evaluate
 
 
 
 class Test(unittest.TestCase):
 
-    def testRule(self):
+    """
+        A list of move counts
+        evaluates to a deterministic result
+    """
+    @data(
         # When all move counts are equal, then all moves win
-        self.assertEqual(rule.evaluate([1, 1, 1]), [1, 1, 1])
-        self.assertEqual(rule.evaluate([3, 3, 3]), [1, 1, 1])
+        ([1, 1, 1], [1, 1, 1]),
+        ([3, 3, 3], [1, 1, 1]),
 
-        # When only one move is present, player always wins
-        self.assertEqual(rule.evaluate([1, 0, 0]), [1, 0, 0])
-        self.assertEqual(rule.evaluate([0, 1, 0]), [0, 1, 0])
-        self.assertEqual(rule.evaluate([0, 0, 1]), [0, 0, 1])
-        self.assertEqual(rule.evaluate([5, 0, 0]), [1, 0, 0])
-        self.assertEqual(rule.evaluate([0, 5, 0]), [0, 1, 0])
-        self.assertEqual(rule.evaluate([0, 0, 5]), [0, 0, 1])
+        # When only one move count is non-zero, then that move wins
+        ([1, 0, 0], [1, 0, 0]),
+        ([0, 1, 0], [0, 1, 0]),
+        ([0, 0, 1], [0, 0, 1]),
+
+        ([5, 0, 0], [1, 0, 0]),
+        ([0, 5, 0], [0, 1, 0]),
+        ([0, 0, 5], [0, 0, 1])
+
+        
+    )
+    @unpack
+    def testRule(self, mcs, result):
+        self.assertEqual(evaluate(mcs), result)
 
 
